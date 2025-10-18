f0d570c84114f1c768ae222a4459e3c40f4e4588
Damien <damien_teng@outlook.com>
2025-10-19 00:17:00 +0800

    wide page

--- monte_carlo/core.py
+++ monte_carlo/core.py
@@ -2,5 +2,7 @@ import streamlit as st
 from heatmap import show_heatmap
 from helpers import st_slider
 
+st.set_page_config(layout="wide")
+
 n_simulations = st_slider("Number of simulations per cell",1,4000,500)
 show_heatmap(n_simulations)
